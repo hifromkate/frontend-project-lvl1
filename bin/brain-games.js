317af7a1bd50d9670d3a877ba1e49285768a0f92
katerina <katyakof@gmail.com>
2020-02-27 12:35:27 +0200

    fix greatestInArray function, fix brain-games

--- bin/brain-games.js
+++ bin/brain-games.js
@@ -1,4 +1,4 @@
 #!/usr/bin/env node
-import greeting from "../src/index.js"
-console.log("Welcome to the Brain Games!");
+import { greeting, userName } from "../src/index.js"
 greeting();
+console.log("Welcome to the Brain Games!");
